b54f93f56b47343b9c4376fedd5d09338659b37e
N16H7H4WK <90453953+CodeAstralIzaX@users.noreply.github.com>
2026-04-06 23:02:53 +0530

    fix: correct import casing for Insurances and Claims pages

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,8 +1,8 @@
 import { createBrowserRouter } from "react-router";
 import { RootLayout } from "./components/RootLayout";
 import Home from "./pages/Home";
-import Insurances from "./pages/insurances";
-import Claims from "./pages/claims";
+import Insurances from "./pages/Insurances";
+import Claims from "./pages/Claims";
 import Contact from "./pages/Contact";
 
 export const router = createBrowserRouter([
